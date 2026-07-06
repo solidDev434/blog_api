3a562c89d5a913986a6f5cbdfa9730b3fd5c744f
Joseph Ibok <josephibok75@gmail.com>
2026-07-06 15:49:39 +0100

    Defined User model + Configured migration using Alembic

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,31 @@
-from sqlmodel import Field, SQLModel
+from enum import Enum
+from sqlmodel import Field, SQLModel, AutoString
+from pydantic import EmailStr
+from datetime import datetime
 
 
-class Hero(SQLModel, table=True):
+class UserRole(str, Enum):
+    READER = "reader"
+    AUTHOR = "author"
+    EDITOR = "editor"
+    ADMIN = "admin"
+
+
+class User(SQLModel, table=True):
     __tablename__ = "users"
+
     id: int | None = Field(default=None, primary_key=True)
-    name: str
-    secret_name: str
-    age: int | None = None
+    username: str = Field(index=True, unique=True, nullable=False)
+    email: EmailStr = Field(sa_type=AutoString, nullable=False)
+    hashed_password: str = Field(nullable=False)
+    role: UserRole = Field(default=UserRole.READER, nullable=False)
+    name: str | None = Field(default=None)
+    avatar_url: str | None = Field(default=None)
+    bio: str | None = Field(default=None)
+
+    is_email_verified: bool = Field(nullable=False, default=False)
+    email_verified_at: datetime = Field(default_factory=lambda: datetime.now())
+
+    created_at: datetime = Field(default_factory=lambda: datetime.now())
+    updated_at: datetime = Field(default_factory=lambda: datetime.now())
+    last_login_at: datetime = Field(default_factory=lambda: datetime.now())
