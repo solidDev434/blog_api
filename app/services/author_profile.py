f5b82e52d1b2612ad1998a4bca501b0a4365e2fe
Joseph Ibok <josephibok75@gmail.com>
2026-07-15 15:26:00 +0100

    Updated and fixed service method issue and added extra validation for create author profile

--- app/services/author_profile.py
+++ app/services/author_profile.py
@@ -1,12 +1,12 @@
 import uuid
 import logging
 from typing import Optional, List
-from sqlmodel import select
+from sqlmodel import select, and_, or_
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from schemas.author_profile import AuthorProfileCreate
 from models.author_profile import AuthorProfile
-from schemas.exceptions import NotFoundError
+from schemas.exceptions import NotFoundError, ConflictError
 
 logger = logging.getLogger(__name__)
 
@@ -62,7 +62,7 @@ class AuthorProfileService:
                 AuthorProfile.user_id == user_id)
             result = await db.execute(statement)
             return result.scalar_one_or_none()
-        except Exception:
+        except:
             logger.exception("Failed to fetch author profile", extra={
                              "user_id": str(user_id)})
             raise
@@ -77,13 +77,21 @@ class AuthorProfileService:
             "user_id": str(user_id)})
 
         try:
+            # Check if profile already exists, throw a conflict error if profile exists else proceed to creating profile
+            statement = select(AuthorProfile).where(
+                and_(AuthorProfile.user_id == user_id, or_(AuthorProfile.pen_name is not None, AuthorProfile.pen_name == payload.pen_name)))
+            existing_profile = await db.execute(statement)
+
+            if existing_profile.scalar_one_or_none():
+                raise ConflictError("Author profile already exists")
+
             data = payload.model_dump(exclude_unset=True)
             profile = AuthorProfile(user_id=user_id, **data)
 
             db.add(profile)
             await db.commit()
             await db.refresh(profile)
-        except Exception:
+        except:
             logger.exception(
                 "Failed to create author profile",
                 extra={"user_id": str(user_id)}
@@ -96,7 +104,7 @@ class AuthorProfileService:
         user_id: uuid.UUID,
         payload: AuthorProfileCreate
     ) -> None:
-        profile = await AuthorProfileService.get_author_profile(db, user_id)
+        profile = await AuthorProfileService.get_authenticated_user_author_profile(db, user_id)
         if not profile:
             logger.info("Author profile update attempted but not found", extra={
                         "user_id": str(user_id)})
@@ -110,7 +118,7 @@ class AuthorProfileService:
             db.add(profile)
             await db.commit()
             await db.refresh(profile)
-        except Exception:
+        except:
             logger.exception("Failed to persist author profile update", extra={
                              "user_id": str(user_id)})
             raise
